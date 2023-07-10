5f5243a8249e30d78f2c8d12aabc57b7a25f7b56
haristariq <tharis715@gmail.com>
2023-07-10 16:52:34 -0700

    Initial Commit

---
+++ Stocipe/StocipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocipeApp.swift
+//  Stocipe
+//
+//  Created by Haris Tariq on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct StocipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
